4affe7fed7e13fbd27f16980cbf8bfd2b3f40283
saidaHF <saida.hf@hotmail.com>
2022-10-07 20:50:24 +0200

    Add files

---
+++ Mob.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+# First, we get the list of animation names from the AnimatedSprite's 
+# frames property. This returns an Array containing all three animation names: ["walk", "swim", "fly"].
+# randi() % n select a random number (0 and n-1)
+func _ready():
+	$AnimatedSprite.playing = true
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
